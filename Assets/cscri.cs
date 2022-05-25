097788e306aa18d789bdb6d85c64115d681496a9
FernandosPerez <82854646+FernandosPerez@users.noreply.github.com>
2022-05-24 22:39:00 -0500

    Update cscri.cs
    ultima revision

--- Assets/cscri.cs
+++ Assets/cscri.cs
@@ -6,11 +6,10 @@ public class cscri : MonoBehaviour
 {
     public GameObject john_idle_0;
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 position = transform.position;
         position.x = john_idle_0.transform.position.x;
         transform.position = position;
-    }
+    } 
 }
